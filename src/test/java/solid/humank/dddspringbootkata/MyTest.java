129ed2dd79d8be1ec4d5fbb58214ca7ec4e33de4
YiKai Kao <yikaikao@amazon.com>
2021-03-17 23:52:33 +0800

    demo for unit test.

---
+++ src/test/java/solid/humank/dddspringbootkata/MyTest.java
@@ -0,0 +1,17 @@
+package solid.humank.dddspringbootkata;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MyTest {
+    @Test
+    void freeTest() {
+        assertEquals(1,2);
+    }
+
+    @Test
+    void newTest() {
+
+    }
+}
