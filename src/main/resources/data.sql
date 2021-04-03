4026e9735000991eb95e4f60233ffdb1de4d1190
river.lin <river.lin@xrspace.io>
2021-04-03 23:51:35 +0800

    add test for createTutorial

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-insert into tutorials (id,title,published,description) values (1,'test1',true,'kimtest');
-insert into tutorials (id,title,published,description) values (2,'test2',true,'fongtest');
-insert into tutorials (id,title,published,description) values (3,'test3',true,'arthurtest');
-insert into tutorials (id,title,published,description) values (4,'test4',true,'rivertest');
+insert into tutorials (id,title,published,description) values (91,'test1',true,'kimtest');
+insert into tutorials (id,title,published,description) values (92,'test2',true,'fongtest');
+insert into tutorials (id,title,published,description) values (93,'test3',true,'arthurtest');
+insert into tutorials (id,title,published,description) values (94,'test4',true,'rivertest');
